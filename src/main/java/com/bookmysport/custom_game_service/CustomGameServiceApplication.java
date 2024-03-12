84adceecb4da40d08feb926f287b741035e6ced1
Anandateertha.dev <vortex1649@gmail.com>
2024-03-12 14:44:32 +0530

    Intialize the template

---
+++ src/main/java/com/bookmysport/custom_game_service/CustomGameServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bookmysport.custom_game_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomGameServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomGameServiceApplication.class, args);
+	}
+
+}
