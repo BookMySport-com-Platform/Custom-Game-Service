26cbeabb143c28c140c78de81191bbcafc817965
Anandateertha.dev <vortex1649@gmail.com>
2024-05-22 10:47:20 +0530

    Remove the CrossOrigin from the controller

--- src/main/java/com/bookmysport/custom_game_service/Controllers/MainController.java
+++ src/main/java/com/bookmysport/custom_game_service/Controllers/MainController.java
@@ -18,7 +18,6 @@ import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -26,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 
 @RestController
 @RequestMapping("api")
-@CrossOrigin(origins = "http://localhost:5173")
 public class MainController {
 
     @Autowired
